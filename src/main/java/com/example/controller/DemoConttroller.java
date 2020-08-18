b074dee7ed18b90d68af9651463dc2af6da1c80f
yangbin <331851841@qq.com>
2020-08-18 12:42:51 +0800

    Initial commit

---
+++ src/main/java/com/example/controller/DemoConttroller.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yangbin
+ * @date 2020/8/18 12:40
+ * @description
+ */
+
+@RestController
+public class DemoConttroller {
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable  String name){
+        return "hello " + name;
+    }
+
+}
